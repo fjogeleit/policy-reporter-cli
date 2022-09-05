8eb924d56a2ce364121a33410bbe11ad436251f7
Frank Jogeleit <frank.jogeleit@web.de>
2022-09-05 16:44:57 +0200

    Add policy filter
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- cmd/results/flags.go
+++ cmd/results/flags.go
@@ -12,6 +12,7 @@ var (
 	results    []string
 	categories []string
 	kinds      []string
+	policies   []string
 )
 
 func sharedFlags(cmd *cobra.Command) *cobra.Command {
@@ -23,6 +24,7 @@ func sharedFlags(cmd *cobra.Command) *cobra.Command {
 	cmd.Flags().StringArrayVar(&results, "result", []string{}, "Filter PolicyReportResults by result")
 	cmd.Flags().StringArrayVarP(&kinds, "kind", "k", []string{}, "Filter PolicyReportResults by kinds (only fullqualified singular kind names are supported)")
 	cmd.Flags().StringArrayVar(&categories, "category", []string{}, "Filter PolicyReportResults by category")
+	cmd.Flags().StringArrayVar(&policies, "policy", []string{}, "Filter PolicyReportResults by policy name")
 	cmd.Flags().StringVar(&groupBy, "group-by", "result", "Group PolicyReportResults by result, category, resource, none")
 
 	return cmd
